e5e3931d5d21aa29290619e4ba0069ff7249b4a2
Dakota <lustdakota14@gmail.com>
2022-03-07 21:18:04 -0700

    Local stress tests finished, ready for clone to EC2

--- server/reviewsAPIserver.js
+++ server/reviewsAPIserver.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import * as newrelic from 'newrelic';
 import { fileURLToPath } from 'url';
 import  { getReviewsdb, saveNewReview, markReviewHelpful, reportReview } from '../db/productReviewsdb.js';
 
--- tests/k6Tests.js
+++
@@ -1,6 +0,0 @@
-import http from 'k6/http';
-import getReviews from './getReviewsk6.js';
-
-export default function () {
-  getReviews();
-};
